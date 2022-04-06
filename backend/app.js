b21dccfe712a1d1c64419cfd4211e25d03c4a055
kapil <katrodiyakapil79@gmail.com>
2022-04-06 12:15:32 +0500

    backed err handling

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error");
 
 app.use(express.json())
  
-//route imports
+//Route imports
 const product = require("./routes/productRoute");
 
 app.use("/api/v1",product);
 
+//Middleware for error
+app.use(errorMiddleware);
 
 module.exports = app
---
+++ backend/middleware/error.js
@@ -0,0 +1,11 @@
+const Errorhander = require("../utils/errorhander");
+
+module.exports = (err,res,req,next)=>{
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    res.status(err.statusCode).json({
+        success:false,
+        error: err,
+    })
+}
