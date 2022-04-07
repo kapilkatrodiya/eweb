e38b4399427e15507abc5a8c1de8c825f260eb85
kapil <katrodiyakapil79@gmail.com>
2022-04-07 23:07:50 +0500

    Day:7, Features Part:1

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 //create product--Admin
 exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
@@ -14,7 +15,8 @@ exports.createProduct = catchAsyncErrors(async (req,res,next)=>{
 
 //get all product
 exports.getAllProducts = async(req,res)=>{
-    const products = await Product.find();
+    const apiFeatures = new ApiFeatures(Product.find(),req.query).search().filter();
+    const products = await apiFeatures.query;
 
     res.status(200).json({
         success:true,
