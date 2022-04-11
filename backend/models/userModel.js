8e274b3e5f25e745f0c89ef89da2e438be2aa304
kapil <katrodiyakapil79@gmail.com>
2022-04-11 21:20:12 +0500

    Day:10,Authentication Part:1

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -50,7 +51,7 @@ userSchema.pre("save",async function(next){
 
 //JWT Token//genrate token and save in cookies,thorugh this server will know the user& it can access the routes
 userSchema.methods.getJWTToken = function(){
-  return jwt.sign({id:this._id},process.env.JWT_SECRET,{
+  return jwt.sign({ id: this._id },process.env.JWT_SECRET,{
     expiresIn:process.env.JWT_EXPIRE,
   });
 };    
@@ -58,6 +59,22 @@ userSchema.methods.getJWTToken = function(){
 //compare password
 userSchema.methods.comparePassword = async function(enteredPassword){
   return await bcrypt.compare(enteredPassword,this.password);
-}
+};
+
+//Generating password reset token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generating Token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hashing and adding resetPasswordToken to userSchema
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
+};  
 
 module.exports = mongoose.model("User",userSchema);
