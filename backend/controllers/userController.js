ff3c79072ba0450b6fb1cb1e3a9141d7824768c7
kapil <katrodiyakapil79@gmail.com>
2022-04-12 22:34:46 +0500

    Day:11,Authentication Part:2, woked on reset password API

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ const ErrorHander = require("../utils/errorhander");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 //Register User
 exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
@@ -58,4 +60,74 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     });
   });
 
+//Forget password
+exports.forgetPassword = catchAsyncErrors(async(req,res,next)=>{
 
+    const user = await User.findOne({email:req.body.email});
+
+    if(!user){
+        return next(new ErrorHander("User not found",404));
+    }
+
+//Get reset password token
+const resetToken = user.getResetPasswordToken();
+
+await user.save({ validateBeforeSave: false});
+
+const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+try {
+    await sendEmail({
+      email: user.email,
+      subject: `Eweb Password Recovery`,
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: `Email sent to ${user.email} successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorHander(error.message, 500));
+  } 
+
+}); 
+
+//Reset password
+exports.resetPassword = catchAsyncErrors(async(req,res,next)=>{
+
+    //creating token hash 
+    const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+    //get user through token hash
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire:{$gt:Date.now()},
+    });
+    //if user not found
+    if(!user){
+        return next(new ErrorHander("Reset password token is invalid or has been expired",400));
+    }
+    //if use found and reset passwppod token was right for matching and change password 
+    if(req.body.password !== req.body.confirmPassword){
+        return next(new ErrorHander("Password does not password",400)); 
+    } 
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user,200,res);
+});
