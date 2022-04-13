fd1bc3592834b8ffe3719b5b6a2e40238cc4bba2
kapil <katrodiyakapil79@gmail.com>
2022-04-13 18:00:13 +0500

    Day:12,Authentication Part:3, Added some Error issues

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,3 +1,4 @@
+const ErrorHander = require("../utils/errorhander");
 const Errorhander = require("../utils/errorhander");
 
 module.exports = (err,req,res,next)=>{
@@ -10,6 +11,23 @@ if(err.name == "CastError"){
     err= new Errorhander(message,400);
 }
 
+//Mongoose duplicate key arror
+if(err.code === 11000){
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHander(message, 400);
+};
+
+//Wrong JWT Token
+if(err.name == "JsonWebTokenError"){
+    const message = `Json Web Token is invalid, Try again`;
+    err= new Errorhander(message,400);
+}
+
+//JWT Expire Error
+if(err.name == "TokenExpiredError"){
+    const message = `Json Web Token is Expired, Try again`;
+    err= new Errorhander(message,400);
+}
 
     res.status(err.statusCode).json({
         success:false,
